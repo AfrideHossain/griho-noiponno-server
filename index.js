585af5b2f18538b1fc9c8803fe04c935abc7008a
AfrideHossain <asafridehossain171@gmail.com>
2024-04-22 19:01:51 +0600

    huge changes

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
+const http = require("http");
 const cors = require("cors");
-const { connectMongo } = require("./mongoDbConnection");
+const { connectMongo, client } = require("./mongoDbConnection");
+const { Server } = require("socket.io");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+
+// verification middlewares
+const verifyJwt = require("./routes/middlewares/verifyJwt");
+const adminVerify = require("./routes/middlewares/adminVerify");
+const { ObjectId } = require("mongodb");
+const { read } = require("fs");
+
+// orders collection
+const ordersCollection = client.db("griho_naipunya").collection("orders");
 
 //call mongodb connection function
 connectMongo();
@@ -20,6 +31,9 @@ app.options("", cors(corsConfig));
 //json middleware
 app.use(express.json());
 
+// create server
+const server = http.createServer(app);
+
 // routes
 app.get("/", (req, res) => {
   res.send("Griho naipunya is running well...");
@@ -34,6 +48,120 @@ app.use("/users", require("./routes/users/manageUsers"));
 // manage payments route
 // app.use("/", require("./routes/managePayments/managePayments"));
 
-app.listen(port, () => {
+/************************
+ * Socket related codes *
+ *************************/
+
+// create io server
+const io = new Server(server, {
+  cors: "*",
+  // allow socket to access cookies
+  // cookie: true,
+});
+
+const clientList = [];
+// write getSocketByUserId function to get socket by user id
+const getSocketByUserId = (userId) => {
+  const client = clientList.find((client) => client.userId === userId);
+  return client ? client.socket : null;
+};
+
+// on connection
+io.on("connection", (socket) => {
+  console.log("A user connected with socket id: ", socket.id);
+  socket.on("loggedin", (userinfo) => {
+    // console.log("event fired with: ", userinfo);
+    // check client is already exist or not. if not then add to client list
+    const client = clientList.find(
+      (client) => client.userId === userinfo.userId
+    );
+    if (!client) {
+      clientList.push({
+        ...userinfo,
+        socket,
+      });
+    }
+    // console.log("client list: ", clientList);
+  });
+  // on new message event
+  socket.on("newMessage", (message) => {
+    console.log("new message event fired with: ", message);
+    // get receiver socket
+    const receiverSocket = getSocketByUserId(message.receiverId);
+    if (receiverSocket) {
+      receiverSocket.emit("newMessage", message);
+    }
+  });
+  socket.on("disconnect", () => {
+    console.log("A user disconnected with socket id: ", socket.id);
+    //remove client from client list
+    const client = clientList.find((client) => client.socket.id === socket.id);
+    if (client) {
+      clientList.splice(clientList.indexOf(client), 1);
+    }
+    // console.log("client list: ", clientList);
+  });
+});
+
+// handle new orders
+app.post("/neworder", verifyJwt, async (req, res) => {
+  const orderDetails = req.body;
+  const newOrder = await ordersCollection.insertOne(orderDetails);
+  if (newOrder.insertedId) {
+    io.emit("neworder", { orederid: newOrder.insertedId });
+    return res.status(200).send(newOrder);
+  }
+  return res.status(400).send({ message: "Something went wrong" });
+});
+
+// fetch orders
+app.get("/getneworders", verifyJwt, adminVerify, async (req, res) => {
+  const allOrders = await ordersCollection
+    .find({}, { sort: { orderedDate: -1 } })
+    .toArray();
+  return res.send(allOrders);
+});
+//fetch pending orders
+app.get("/pendingorders", verifyJwt, adminVerify, async (req, res) => {
+  const pendingOrders = await ordersCollection
+    .find({ status: "pending" }, { sort: { orderedDate: -1 } })
+    .toArray();
+  return res.send(pendingOrders);
+});
+// get order
+app.get("/order/:orderId", verifyJwt, adminVerify, async (req, res) => {
+  const { orderId } = req.params;
+  const order = await ordersCollection.findOne({ _id: new ObjectId(orderId) });
+  if (order) {
+    return res.send(order);
+  }
+  return res.status(400).send({ message: "Something went wrong" });
+});
+
+// TODO: update order status
+app.patch("/orderstatus", async (req, res) => {
+  const { orderId } = req.query;
+  const { status } = req.body;
+  // console.log({ id, status });
+  try {
+    const updateOrderStatus = await ordersCollection.updateOne(
+      {
+        _id: new ObjectId(orderId),
+      },
+      { $set: { status } }
+    );
+    if (updateOrderStatus.modifiedCount > 0) {
+      return res
+        .status(200)
+        .json({ success: true, msg: "order status changed" });
+    } else {
+      return res.status(404).send("order not found");
+    }
+  } catch (error) {
+    return res.status(500).send("Internal server error");
+  }
+});
+
+server.listen(port, () => {
   console.log(`Griho naipunya listening on port ${port}`);
 });
--- routes/users/manageUsers.js
+++ routes/users/manageUsers.js
@@ -27,9 +27,13 @@ router.patch("/updateuser", verifyJwt, async (req, res) => {
       }
     );
     if (updateUser.modifiedCount > 0) {
-      return res.status(200).send({ message: "Update Successfully!" });
+      return res
+        .status(200)
+        .send({ success: true, message: "Update Successfully!" });
     } else {
-      return res.status(400).send({ message: "No changes detected." });
+      return res
+        .status(400)
+        .send({ success: false, message: "No changes detected." });
     }
   } catch (error) {
     return res.send(error.message);
@@ -84,16 +88,75 @@ router.patch("/addtocart/:id", verifyJwt, async (req, res) => {
 });
 
 // route 4: remove from cart
-router.delete("/removefromcart/:index", verifyJwt, async (req, res) => {
+router.delete("/removefromcart/:id", verifyJwt, async (req, res) => {
   let userEmail = req.user.email;
-  let removedIndex = parseInt(req.params.index);
-  let cartData = await carts_collections
-    .findOneAndUpdate(
-      { userEmail },
-      { $pull: { items: { _id: removedIndex } } }
-    )
-    .exec();
-  return res.status(200).send(cartData.value.items);
+  let removedId = req.params.id;
+
+  try {
+    const existingCart = await users_collections.findOne(
+      { email: userEmail },
+      { projection: { cart: 1 } }
+    );
+    // check if the item to be deleted exists in the cart or not
+    /* if (existingCart.cart.some((item) => item.productid === removedId)) {
+    const updatedCart = existingCart.cart.filter((item) => item.productid !== removedId);
+    await users_collections.updateOne(
+      { email: userEmail },
+      { $set: { cart: updatedCart } }
+    ).then(() => {
+      console.log(`Item with ID ${removedId} has been successfully removed from the Cart.`);
+      return res.status(200).json("Successfully Removed");
+    }).catch((err) => {
+      console.error(err);
+      return res.status(500).json("Server Error");
+    })
+  } else {
+    return res.status(400).json("The Item is Not Present in Your Cart.");
+  } */
+    console.log("User's existing cart=> ", existingCart);
+    const updatedCart = existingCart.cart.filter(
+      (item) => item.productid !== removedId
+    );
+    console.log("Updated cart => ", updatedCart);
+
+    const updateUserCart = await users_collections.updateOne(
+      { email: userEmail },
+      { $set: { cart: updatedCart } }
+    );
+    console.log(updateUserCart);
+    if (updateUserCart.modifiedCount > 0) {
+      return res
+        .status(200)
+        .json({
+          success: true,
+          message: `Product has been removed from your cart`,
+        });
+    } else {
+      return res
+        .status(404)
+        .json({ success: false, message: "No product found with provided id" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal server error");
+  }
+
+  return res.status(200).send("Item has been successfully removed");
+});
+
+//  route 5: get cart of a specific user
+router.get("/cart", verifyJwt, async (req, res) => {
+  const user = req.user;
+  try {
+    const cartData = await users_collections.findOne(
+      { email: user.email },
+      { projection: { cart: 1 } }
+    );
+
+    return res.send(cartData);
+  } catch (err) {
+    return res.status(403).json({ msg: "Error getting data" });
+  }
 });
 
 module.exports = router;
